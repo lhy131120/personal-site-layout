ba794d01186019c1012f19b12d8ddbda5be09546
Ray <lhy131120@gmail.com>
2025-05-07 17:39:58 +0800

    finished layout

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,48 +1,56 @@
+// swiper 
 import Swiper from 'swiper';
 import { Navigation, EffectFade } from 'swiper/modules';
 import 'swiper/swiper-bundle.css';
 
+// gsap
+
+import {gsap} from "gsap"; 
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { Observer } from "gsap/Observer";
+gsap.registerPlugin(Observer, ScrollTrigger);
+
+// css
 import '../style/style.css';
 
-// import viteLogo from '/vite.svg'
-// import { setupCounter } from './counter.js'
+document.addEventListener('DOMContentLoaded', function() {
 
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//     <a href="https://vite.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite logo to learn more
-//     </p>
-//   </div>
-// `
-
-// setupCounter(document.querySelector('#counter'))
-
-document.addEventListener('DOMContentLoaded', function () {
-
-  if(document.querySelector(".blog")) {
-    const swiper = new Swiper('.blog .swiper', {
-      modules: [Navigation, EffectFade],
-      slidesPerView: 1,
-      spaceBetween: 24,
-      breakpoints: {
-        992: {
-          slidesPerView: 3,
-          spaceBetween: 24,
-          effect: 'slide',
-        },
-      },
-      navigation: {
-        nextEl: '.blog .swiper-button-next',
-        prevEl: '.blog .swiper-button-prev',
-      },
-    })
-    
+  if(document.querySelector('.blog')) {
+    const swiper = new Swiper(".blog .swiper", {
+			modules: [Navigation, EffectFade],
+			slidesPerView: 1,
+			spaceBetween: 24,
+			breakpoints: {
+				992: {
+					slidesPerView: 3,
+					spaceBetween: 24,
+					effect: "slide",
+				},
+			},
+			navigation: {
+				nextEl: ".blog .swiper-button-next",
+				prevEl: ".blog .swiper-button-prev",
+			},
+		});
   }
-});
+
+  const backTopBtn = document.querySelector(".back-top");
+	if (backTopBtn) {
+		window.addEventListener("scroll", function () {
+			if (window.scrollY > 200) {
+				backTopBtn.classList.add("show");
+			} else {
+				backTopBtn.classList.remove("show");
+			}
+		});
+
+		backTopBtn.addEventListener("click", function () {
+			window.scrollTo({
+				top: 0,
+				behavior: "smooth",
+			});
+		});
+	} else {
+		console.warn("Back to Top button not found in the DOM.");
+	}
+})
